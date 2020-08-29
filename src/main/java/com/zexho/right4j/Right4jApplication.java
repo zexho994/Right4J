2956e5f7607a4118a98667d0a05809689eb82802
zouzhihao <1035281205@qq.com>
2020-08-29 15:32:43 +0800

    init

---
+++ src/main/java/com/zexho/right4j/Right4jApplication.java
@@ -0,0 +1,13 @@
+package com.zexho.right4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Right4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Right4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zexho/right4j/Right4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zexho.right4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Right4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
